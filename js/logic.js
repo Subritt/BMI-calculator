8ccad046707dab509f62c38291160a2330034b75
Subritt <subrittburlakoti@gmail.com>
2020-07-14 23:56:03 +0545

    wrote a function to insert HTML to display BMI details

--- js/logic.js
+++ js/logic.js
@@ -6,7 +6,9 @@ function getValues() {
     var height = document.getElementById('height').value;
     console.log("Your height: " + height + " meter" + "\nYour Weight: " + weight + " kg");
     
-    calculateBmi(weight, height);
+    var BMI = calculateBmi(weight, height);
+    console.log("Your BMI: " + BMI);
+    addElement(weight, height, BMI);
 }
 
 /**
@@ -14,5 +16,22 @@ function getValues() {
  */
 function calculateBmi(weight, height){
     var BMI = weight/(height ** 2);
-    console.log("Your BMI: " + BMI.toFixed(2));
+    return BMI.toFixed(2);
+}
+
+/**
+ * function to create dynamic emelent
+ */
+function addElement(weight, height, BMI){
+    var row = document.createElement('div');
+    row.className = 'row';
+
+    row.innerHTML = `<div id="display-details" class="col s12">
+                        <h5>Your Details:</h5>
+                        <p id="bmi-details">Your Height: ${height}</p>
+                        <p id="bmi-details">Your Weight: ${weight}</p>
+                        <p id="bmi-details">Your BMI: ${BMI}</p>
+                    </div><!--CLOSE COLUMN-->`;
+
+    document.getElementById('main-container').appendChild(row);
 }
