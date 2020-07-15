e98e249b77538dbdeee00e6bc20ace3e5247f6c3
Subritt <subrittburlakoti@gmail.com>
2020-07-15 18:00:35 +0545

    added bmi result

--- js/logic.js
+++ js/logic.js
@@ -6,9 +6,9 @@ function getValues() {
     var height = document.getElementById('height').value;
     console.log("Your height: " + height + " meter" + "\nYour Weight: " + weight + " kg");
     
-    var BMI = calculateBmi(weight, height);
-    console.log("Your BMI: " + BMI);
-    addElement(weight, height, BMI);
+    var bmiResult = calculateBmi(weight, height);
+    console.log("Your BMI: " + bmiResult[0]);
+    addElement(weight, height, bmiResult);
 
     document.getElementById('weight').value = "";
     document.getElementById('height').value = "";
@@ -19,13 +19,28 @@ function getValues() {
  */
 function calculateBmi(weight, height){
     var BMI = weight/(height ** 2);
-    return BMI.toFixed(2);
+    return [BMI.toFixed(2), checkRange(BMI)];
+}
+
+/**
+ * function to check BMI range
+ */
+function checkRange(BMI) {
+    if (BMI < 18.5) {
+        return "You fall under UNDERWEIGHT Range!";
+    }else if(BMI >= 18.5 && BMI < 25){
+        return "You fall under NORMAL Range :)";
+    }else if(BMI >= 25 && BMI < 30){
+        return "You fall under OVERWEIGHT Range!";
+    }else{
+        return "You fall under OBESE Range";
+    }
 }
 
 /**
  * function to create dynamic emelent
  */
-function addElement(weight, height, BMI){
+function addElement(weight, height, bmiResult){
     var row = document.createElement('div');
     row.className = 'row';
 
@@ -33,9 +48,10 @@ function addElement(weight, height, BMI){
                         <div class="card blue-grey lighten-5">
                             <div class="card-content">
                                 <span class="card-title"><b>Your Details:</b><hr></span>
+                                <p id="bmi-details" class="right"><b>${bmiResult[1]}</b></p>
                                 <p id="bmi-details">Your Height: <b>${height}</b></p>
                                 <p id="bmi-details">Your Weight: <b>${weight}</b></p>
-                                <p id="bmi-details">Your BMI: <b>${BMI}</b></p>
+                                <p id="bmi-details">Your BMI: <b>${bmiResult[0]}</b></p>
                             </div>
                         </div>
                     </div>`;
